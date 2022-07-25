6f2e59565a7898395264b623578ed1389a5b9247
lexur <2259334682@qq.com>
2022-07-25 21:40:08 +0800

    创建SpringBoot项目目录结构

---
+++ src/main/java/cc/lexur/sunset/SunsetApplication.java
@@ -0,0 +1,17 @@
+package cc.lexur.sunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.print.SunAlternateMedia;
+
+/**
+ * @Description: cc.lexur.sunset
+ * @Auther: lexur
+ * @Date: 2022/7/25
+ */
+@SpringBootApplication
+public class SunsetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SunsetApplication.class, args);
+    }
+}
